855582c9c4cd108fd23d0626d7a9436078be9721
Tanvir Fuad <tanvir23anan@gmail.com>
2024-04-20 17:38:40 +0600

    categories added

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -13,28 +13,31 @@ class FrontendController extends Controller
     // home page 
     public function index(){
 
-        $showCategoryButton = true;
-        $heroSliders = HeroSlider::select(['title','image'])->oldest()->take(2)->get();
+        $showCategoryButton     =       true;
+        $heroSliders            =       HeroSlider::select(['title','image'])->oldest()->take(2)->get();
+        $featuredCategories     =       Category::with(['products' => function ($query){
+                                            $query->where("status", 1);
+                                        }])
+                                        ->latest()
+                                        ->get();
         
-        return view('frontend.index', compact('showCategoryButton', 'heroSliders'));
+        return view('frontend.index', compact('showCategoryButton', 'heroSliders','featuredCategories'));
 
     }
 
     public function getAllCategories(){
-        $totalCategories = Category::count();
-        $categoriesLeft = Category::latest()->take(5)->get();
-        $categoriesRight = Category::latest()->skip(5)->take(5)->get();
+        $totalCategories        =       Category::count();
+        $categoriesLeft         =       Category::latest()->take(5)->get();
+        $categoriesRight        =       Category::latest()->skip(5)->take(5)->get();
 
         return response()->json([
-            'total'     =>  $totalCategories,
-            'left'      =>  $categoriesLeft,
-            'right'     =>  $categoriesRight
+            'total'             =>      $totalCategories,
+            'left'              =>      $categoriesLeft,
+            'right'             =>      $categoriesRight
         ]);
         
     }
-
- 
-
+    
     public function getAllBanners(){
         $banners = Banner::oldest()->take(3)->get();
         return response()->json(['banner' => $banners]);
@@ -43,11 +46,13 @@ class FrontendController extends Controller
     // display all category 
     public function categoryList(){
 
-        $showCategoryButton = false;
-        $pageTitle = "All Categories";
-        $allCategories = Category::with(['products' => function ($query){
-            $query->where("status", 1)->get();
-        }])->latest()->get();
+        $showCategoryButton     =        false;
+        $pageTitle              =        "All Categories";
+        $allCategories          =        Category::with(['products' => function ($query){
+                                            $query->where("status", 1)->get();
+                                        }])
+                                        ->latest()
+                                        ->get();
 
         return view('frontend.category_list', compact('pageTitle', 'allCategories', 'showCategoryButton'));
     }
@@ -55,13 +60,19 @@ class FrontendController extends Controller
     // display all products of a specific category
     public function productList($category_id){
 
-        $showCategoryButton = false;
+        $showCategoryButton     =       false;
 
-        $categoryWithProduct = Category::with(['products' => function ($query){
-            $query->where('status', 1)->get();
-        }])->where('id', $category_id)->first();
+        $categoryWithProduct    =       Category::with(['products' => function ($query){
+                                            $query->where('status', 1)->get();
+                                        }])
+                                        ->where('id', $category_id)
+                                        ->first();
 
-        $restCategories = Category::with('products')->where('id', '!=', $category_id)->latest()->get()->take(6);
+        $restCategories         =       Category::with('products')->where('id', '!=', $category_id)
+                                        ->latest()
+                                        ->get()
+                                        ->take(6);
+        
        
         return view('frontend.product_list', compact('categoryWithProduct', 'restCategories', 'showCategoryButton'));
 
